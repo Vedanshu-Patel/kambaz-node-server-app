54029b99ed787727ce6d76373144983fcd74151a
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-04-08 17:10:29 -0400

    enrollments checking

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,15 +1,20 @@
 import Database from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
-
+import model from "./model.js";
 
 export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+  // const { enrollments } = Database;
+  // enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
+  return model.create({ user, course, _id: `${user}-${course}` });
 };
 export function findAllEnrollments(){
-    return Database.enrollments;
+    // return Database.enrollments;
+    return model.find();
 };
 export function unenrollUserInCourse(userId, courseId){
-  const {enrollments} = Database;
-  Database.enrollments = enrollments.filter((enrollment)=>!(userId===enrollment.user &&  courseId===enrollment.course ));
+  // const {enrollments} = Database;
+  // Database.enrollments = enrollments.filter((enrollment)=>!(userId===enrollment.user &&  courseId===enrollment.course ));
+  return model.deleteOne({ user, course });
 }
+
+ 
