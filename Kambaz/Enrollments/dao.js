7409eaf0146695fea903acd1b7db69505dca6224
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-04-08 17:18:41 -0400

    Back to correct version

--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -3,18 +3,15 @@ import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
 
 export function enrollUserInCourse(userId, courseId) {
-  // const { enrollments } = Database;
-  // enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
-  return model.create({ user, course, _id: `${user}-${course}` });
+  const { enrollments } = Database;
+  enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
 };
 export function findAllEnrollments(){
-    // return Database.enrollments;
-    return model.find();
+    return Database.enrollments;
 };
 export function unenrollUserInCourse(userId, courseId){
-  // const {enrollments} = Database;
-  // Database.enrollments = enrollments.filter((enrollment)=>!(userId===enrollment.user &&  courseId===enrollment.course ));
-  return model.deleteOne({ user, course });
+  const {enrollments} = Database;
+  Database.enrollments = enrollments.filter((enrollment)=>!(userId===enrollment.user &&  courseId===enrollment.course ));
 }
 
  
