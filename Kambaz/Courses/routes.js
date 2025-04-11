3e1c6833d6af8064c25befddffe224bce39f11cd
Vedanshu Patel <79440084+Vedanshu-Patel@users.noreply.github.com>
2025-04-11 17:46:04 -0400

    People

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,5 +1,6 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js"
 export default function CourseRoutes(app) {
   app.post("/api/courses/:courseId/modules",async (req, res) => {
     const { courseId } = req.params;
@@ -37,5 +38,10 @@ export default function CourseRoutes(app) {
     res.json(course);
   });
  
-
+  const findUsersForCourse = async (req, res) => {
+    const { cid } = req.params;
+    const users = await enrollmentsDao.findUsersForCourse(cid);
+    res.json(users);
+  };
+  app.get("/api/courses/:cid/users", findUsersForCourse);
 }
--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,7 +1,7 @@
 // import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import model from "./model.js";
-
+import userModel from "../Users/model.js"
 export function enrollUserInCourse(userId, courseId) {
   console.log(courseId)
   // const { enrollments } = Database;
@@ -23,6 +23,19 @@ export function unenrollAllInCourse(courseId){
 return model.deleteMany({course:courseId})
 }
 
+export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  const userIds = enrollments.map((enrollment) => enrollment.user);
+  let result = [];
+  const users = await userModel.find({ _id: { $in: userIds } })
+  .then(users => {
+    
+    result = users
+  })
+  .catch(error => {
+    console.error('Error fetching users:', error);
+  });
 
-
+  return result;
+ }
  
